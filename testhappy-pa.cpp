40031d0eb3e1d03eff1421ab4fcd2f57f0799fd0
folkert van heusden <mail@vanheusden.com>
2022-05-04 19:34:52 +0200

    record sound in a thread

--- testhappy-pa.cpp
+++ testhappy-pa.cpp
@@ -2,6 +2,7 @@
 
 // This program interfaces to pulse-audio.
 
+#include <cstring>
 #include <signal.h>
 #include <unistd.h>
 #include <pulse/error.h>
@@ -11,8 +12,16 @@
 
 
 typedef struct {
-	pa_simple *record;  // from pa to voip
-	pa_simple *play;  // from voip to pa
+	std::thread *rec_th;  // recorder thread
+
+	int          buffer_length;
+	short       *buffer[2];
+	int          buffer_selection;
+
+	pa_simple   *record;  // from pa to voip
+	pa_simple   *play;  // from voip to pa
+
+	std::atomic_bool *stop_flag;
 } pa_sessions_t;
 
 const pa_sample_spec ss = {
@@ -37,6 +46,25 @@ bool cb_new_session(sip_session_t *const session)
 
 	p->play   = pa_simple_new(NULL, "libHappy", PA_STREAM_PLAYBACK, NULL, "playback", &ss, NULL, NULL, &error);
 
+	p->buffer_length    = session->samplerate * 20 / 1000;
+
+	p->buffer[0]        = new short[p->buffer_length];
+	p->buffer[1]        = new short[p->buffer_length];
+
+	p->buffer_selection = 0;
+
+	p->stop_flag        = &session->stop_flag;
+
+	p->rec_th = new std::thread([p]() {
+			while(!*p->stop_flag) {
+				int error = 0;  // TODO handle errors
+				pa_simple_read(p->record, p->buffer[1 - p->buffer_selection], p->buffer_length * sizeof(short), &error);
+
+				// TODO locking
+				p->buffer_selection = 1 - p->buffer_selection;
+			}
+		});
+
 	return true;
 }
 
@@ -56,14 +84,12 @@ bool cb_recv(const short *const samples, const size_t n_samples, sip_session_t *
 // the peer
 bool cb_send(short **const samples, size_t *const n_samples, sip_session_t *const session)
 {
-	*n_samples = 10 * 44100 / 1000;
-
-	*samples = new short[*n_samples];
-
 	pa_sessions_t *p = reinterpret_cast<pa_sessions_t *>(session->private_data);
 
-	int error = 0;  // TODO handle errors
-	pa_simple_read(p->record, *samples, *n_samples * sizeof(short), &error);
+	*n_samples = p->buffer_length;
+	*samples   = new short[*n_samples];
+
+	memcpy(*samples, p->buffer[p->buffer_selection], p->buffer_length * sizeof(short));
 
 	return true;
 }
@@ -75,6 +101,11 @@ void cb_end_session(sip_session_t *const session)
 	printf("cb_end_session\n");
 
 	pa_sessions_t *p = reinterpret_cast<pa_sessions_t *>(session->private_data);
+
+	session->stop_flag = true;
+
+	p->rec_th->join();
+	delete p->rec_th;
 
 	pa_simple_free(p->play);
 
@@ -96,7 +127,7 @@ int main(int argc, char *argv[])
 	setlog("testhappy.log", debug, debug);
 
 	// remote ip (IP address of upstream asterisk server), my extension-number, my password, my ip, my sip port, samplerate-used-by-callbacks, [callbacks...]
-	sip s("192.168.64.1", "9999", "1234", "192.168.65.201", 5060, 60, 44100, cb_new_session, cb_recv, cb_send, cb_end_session);
+	sip s("192.168.64.1", "9999", "1234", "192.168.65.158", 5060, 60, 44100, cb_new_session, cb_recv, cb_send, cb_end_session);
 
 	// do whatever you like here
 	pause();
